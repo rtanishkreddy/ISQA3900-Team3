36950ab22f79b81c00181eedecd5143599a80b43
Joshua Kroupa <jkroupa103@gmail.com>
2019-07-29 11:07:01 -0500

    Update settings.py

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -129,8 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 LOGIN_REDIRECT_URL = '/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 CART_SESSION_ID = 'cart'
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
 
 # Braintree settings
